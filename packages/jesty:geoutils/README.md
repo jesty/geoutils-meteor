4e0db83ff54bccd184058be468f63ed16051c621
Davide <davidecerbo@gmail.com>
2015-06-26 00:04:47 +0200

    update docs

---
+++ packages/jesty:geoutils/README.md
@@ -0,0 +1,2 @@
+This project is useful to calculate distance between two points, compute heading, and more. To read the full documentation please visit GitHub project.
+
--- packages/jesty:geoutils/package.js
+++ packages/jesty:geoutils/package.js
@@ -1,5 +1,6 @@
 Package.describe({
   summary: "Some utilities to handle with geo coordinates in Meteor.",
+  documentation: "This project is useful to calculate distance between two points, compute heading, and more. To read the full documentation please visit GitHub project."
   version: "0.1.0",
   name: "jesty:geoutils",
   git: "https://github.com/jesty/geoutils-meteor.git",
